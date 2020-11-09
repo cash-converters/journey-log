854a6e708fcc58ff178f315f469a343a093a309c
Andreas Rutjens <arutjens@cash-converters.es>
2020-11-09 14:07:00 +0100

    add requestshema

--- public/activity.js
+++ public/activity.js
@@ -1,14 +1,26 @@
 const connection = new Postmonger.Session();
 
+let schema;
+
 connection.trigger('ready');
 
 connection.on('initActivity', function(data) {
     console.log(data);
     document.getElementById('configuration').value = JSON.stringify(data, null, 2);
-})
+});
 
 connection.on('clickedNext', function() {
     const configuration = JSON.parse(document.getElementById('configuration').value);
     console.log(configuration);
     connection.trigger('updateActivity', configuration);
-})
+});
+
+
+connection.on('requestedSchema', function (data) {
+    if (data.error) {
+        console.error(data.error);
+    } else {
+        schema = data['schema'];
+    }
+    console.log('*** Schema ***', JSON.stringify(schema));
+});
